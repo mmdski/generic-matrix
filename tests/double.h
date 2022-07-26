876a9f99d35f521316ab65e5637be1302b9ce896
Marian Domanski <mmdski@gmail.com>
2022-07-26 13:14:26 -0500

    add tests

---
+++ tests/double.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gmatrix.h>
+
+static void
+dbl_print(void *a_ptr) {
+  printf("%10g", *((double *) a_ptr));
+}
+
+static void
+dbl_zero(void *a_ptr) {
+  *((double *) a_ptr) = 0;
+}
+
+static void
+dbl_one(void *a_ptr) {
+  *((double *) a_ptr) = 1;
+}
+
+static void
+dbl_add(void *a_ptr, void *b_ptr, void *sum_ptr) {
+  *((double *) sum_ptr) = *((double *) a_ptr) + *((double *) b_ptr);
+}
+
+static void
+dbl_mult(void *a_ptr, void *b_ptr, void *prod_ptr) {
+  *((double *) prod_ptr) = *((double *) a_ptr) * *((double *) b_ptr);
+}
+
+static size_t dbl_width = sizeof(double);
+
+static ElementOperations dbl_ops = {
+    dbl_zero, dbl_one, dbl_print, dbl_add, dbl_mult};

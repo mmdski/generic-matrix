7c14a07552a6dedf99f53bdbb3f804dc9b3d5a42
Marian Domanski <mmdski@gmail.com>
2022-08-03 21:15:14 -0500

    add example 1.6.1

---
+++ app/chapter1/example1.6.1.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+#include <gmat/gauss.h>
+#include <gmat/matrix.h>
+
+using namespace std;
+using namespace gmat;
+
+int
+main(void) {
+
+  Matrix<double> a(2, 2, vector<double>{0.835, 0.667, 0.333, 0.266});
+  Matrix<double> b(2, 1, vector<double>{0.168, 0.067});
+
+  cout << "Ax=B" << endl << "A:" << endl << a << endl << "b:" << endl << b;
+
+  Matrix<double> x = gauss::Solve(a, b);
+  cout << "x:" << endl << x;
+
+  b(2, 1) = 0.066;
+
+  x = gauss::Solve(a, b);
+  cout << "b:" << endl << b << endl << "x:" << endl << x;
+
+  return 0;
+}
